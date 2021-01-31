38ebc1c154c8c63a5bb059156df09482c8f82ffe
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-01-31 14:32:34 -0300

    Churrascometro

---
+++ churrascometro/scripts/script.js
@@ -0,0 +1,45 @@
+let inputAdultos = document.getElementById("adultos")
+let inputCriancas = document.getElementById("criancas")
+let inputDuracao = document.getElementById("duracao")
+
+let resultado = document.getElementById("resultado")
+
+function calcular(){
+    console.log("calculando")
+
+    let adultos = inputAdultos.value
+    let criancas = inputCriancas.value
+    let duracao = inputDuracao.value
+
+    let qtdTotalCarne = carnePP(duracao) * adultos + (carnePP(duracao)/2 * criancas)
+    let qtdTotalCerveja = cervejaPP(duracao) * adultos
+    let qtdTotalBebidas = bebidasPP(duracao) * adultos + (bebidasPP(duracao)/2 * criancas)
+
+    resultado.innerHTML = `<p>${qtdTotalCarne}g de Carne</p>`
+    resultado.innerHTML += `<p>${qtdTotalCerveja}ml de Cerveja</p>`
+    resultado.innerHTML += `<p>${qtdTotalBebidas}g de Bebidas</p>`
+}
+
+function carnePP(duracao){
+    if(duracao >= 6){
+        return 650
+    } else{
+        return 400
+    }
+}
+
+function cervejaPP(duracao){
+    if(duracao >= 6){
+        return 2000
+    } else{
+        return 1200
+    }
+}
+
+function bebidasPP(duracao){
+    if(duracao >= 6){
+        return 1500
+    } else{
+        return 1000
+    }
+}
